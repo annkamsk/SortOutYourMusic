3ebb711dc52c7c409c65733649c7b4aca8180081
annkamsk <an.kramarska@gmail.com>
2019-11-29 17:03:01 +0100

    Updates

---
+++ precache-manifest.69c58968776bbfa085fb4e564ac17146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1086801a5e8bd290fffa2493fcc53c5f",
+    "url": "/SortOutYourMusic/index.html"
+  },
+  {
+    "revision": "def546cf948209163a72",
+    "url": "/SortOutYourMusic/static/css/main.91d8b251.chunk.css"
+  },
+  {
+    "revision": "3e890208409a585c1f99",
+    "url": "/SortOutYourMusic/static/js/2.1135f4c6.chunk.js"
+  },
+  {
+    "revision": "def546cf948209163a72",
+    "url": "/SortOutYourMusic/static/js/main.a815de1a.chunk.js"
+  },
+  {
+    "revision": "55935d2fbd73d85ca898",
+    "url": "/SortOutYourMusic/static/js/runtime~main.959dc711.js"
+  }
+]);
